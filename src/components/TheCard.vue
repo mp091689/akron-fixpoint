7f085e3e7de04437c57a1b06fb73fcd27c7fc6a4
Mykyta <mp091689@gmail.com>
2025-12-11 09:22:48 -0500

    Refactor TheCard component to support dynamic backgrounds and integrate it into TheWelcome with new service cards.

--- src/components/TheCard.vue
+++ src/components/TheCard.vue
@@ -1,11 +1,33 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+import {computed} from 'vue';
 
+interface CardProps {
+  title?: string;
+  description?: string;
+  backgroundImg?: string;
+}
+
+const props = defineProps<CardProps>();
+const bgStyle = computed(() => {
+  if (!props.backgroundImg) return {};
+  return {
+    backgroundImage: `url(${new URL(props.backgroundImg, import.meta.url).href})`,
+  };
+});
 </script>
 
 <template>
-
+  <div class="mt-5">
+    <div
+      :style="bgStyle"
+      class="card-header rounded-2xl w-full h-64 bg-cover bg-center flex flex-col justify-end p-8 ">
+      <h4 class="text-white font-normal tracking-wide text-shadow-xs">{{ props.title }}</h4>
+    </div>
+    <div class="card-content">
+      <slot></slot>
+    </div>
+    <div v-if="$slots.footer" class="card-footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,15 +1,27 @@
 <script lang="ts" setup>
 import bgImage from '@/assets/repair-service-_v_.jpg'
+import bgCardMac from '@/assets/servises/mac.webp'
+import bgCardPhone from '@/assets/servises/phone.webp'
+import bgCardConsole from '@/assets/servises/console.webp'
+import bgCardTV from '@/assets/servises/tv.webp'
+import bgCardTablet from '@/assets/servises/tablet.webp'
+import TheCard from "@/components/TheCard.vue";
 </script>
 
 <template>
-  <div :style="{ backgroundImage: `url(${bgImage})` }"
-       class="rounded-2xl w-full h-10/12 bg-cover bg-center flex flex-col items-center p-8">
-    <div class="flex flex-col">
-      <h2 class="text-left font-bold text-4xl text-white text-shadow-2xs">FixPoint</h2>
-      <p class="text-right text-yellow-300 text-shadow-2xs">Akron</p>
+    <div :style="{ backgroundImage: `url(${bgImage})` }"
+         class="rounded-2xl w-full h-10/12 bg-cover bg-center flex flex-col items-center p-8">
+      <div class="flex flex-col">
+        <h2 class="text-left font-bold text-4xl text-white text-shadow-2xs">FixPoint</h2>
+        <p class="text-right text-yellow-400 text-shadow-2xs">Akron</p>
+      </div>
+      <p class="text-center font-thin leading-8">Professional repair of smartphones, tablets, laptops,
+        gaming consoles, and other electronics
+        in Akron.</p>
     </div>
-    <p class="text-center font-thin leading-8">Professional repair of smartphones, tablets, laptops, gaming consoles, and other electronics
-      in Akron.</p>
-  </div>
+    <TheCard :background-img="bgCardPhone" title="Cellphone service" />
+    <TheCard :background-img="bgCardTablet" title="Tablet service" />
+    <TheCard :background-img="bgCardMac" title="Laptop service" />
+    <TheCard :background-img="bgCardConsole" title="Gaming console service" />
+    <TheCard :background-img="bgCardTV" title="TV service" />
 </template>
